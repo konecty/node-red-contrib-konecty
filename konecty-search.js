04149aac04ea11a0677349df31f2e306b80e2c34
Derotino Silveira <silveirado@gmail.com>
2019-12-10 15:30:33 -0300

    Add sort fields

--- konecty-search.js
+++ konecty-search.js
@@ -1,121 +1,168 @@
-const axios_instance = require('axios');
-const moment = require('moment');
+const axios_instance = require("axios");
+const moment = require("moment");
 
 module.exports = function(RED) {
-	function KonectySearchNode(config) {
-		
-		RED.nodes.createNode(this, config);
-		let node = this;
-		let flow = node.context().flow;
-		let global = node.context().global;
-		node.server = RED.nodes.getNode(config.server);
+  function KonectySearchNode(config) {
+    RED.nodes.createNode(this, config);
+    let node = this;
+    let flow = node.context().flow;
+    let global = node.context().global;
+    node.server = RED.nodes.getNode(config.server);
 
-		node.on('input', function(msg) {
-			// FILTER EVALUATION
-			let filter = JSON.parse(config.filter);
-			for (let i = 0, j = filter.conditions.length; i < j; i++) {
-                let c = filter.conditions[i];
-                
-                if (c.value1 && c.value1.length) {
-                    c.value1 = RED.util.evaluateNodeProperty(c.value1, c.value1Type, this, msg);
-                }
-                console.log("TCL: KonectySearchNode -> c.value1", c.value1)
+    node.on("input", function(msg) {
+      // FILTER EVALUATION
+      let filter = JSON.parse(config.filter);
+      for (let i = 0, j = filter.conditions.length; i < j; i++) {
+        let c = filter.conditions[i];
 
-				if (!!c.value2) {
-					c.value2 = RED.util.evaluateNodeProperty(c.value2, c.value2Type, this, msg);
-				}
-				
-				if (c.operator === 'lookup') {
-					c.term += '._id';
-					c.operator = 'equals';
-					c.value = c.lookupId;
-				} else if (c.operator === 'between') {
-					c.value = {};
-					if (c.value1) {
-						if (c.fieldType == 'dateTime') c.value['greater_or_equals'] = { $date: moment(c.value1, 'YYYY-MM-DD HH:mm').format() };
-						else if (c.fieldType == 'date') c.value['greater_or_equals'] = { $date: moment(c.value1, 'YYYY-MM-DD').format() };
-                        else if (c.fieldType == 'money' || c.fieldType == 'number') c.value['greater_or_equals'] = Number(c.value1);
-                        else c.value['greater_or_equals'] = c.value1;
-					}
-					if (c.value2) {
-						if (c.fieldType == 'dateTime') c.value['less_or_equals'] = { $date: moment(c.value2, 'YYYY-MM-DD HH:mm').format() };
-						else if (c.fieldType == 'date') c.value['less_or_equals'] = { $date: moment(c.value2, 'YYYY-MM-DD').format() };
-                        else if (c.fieldType == 'money' || c.fieldType == 'number') c.value['less_or_equals'] = Number(c.value2);
-                        else c.value['less_or_equals'] = c.value2;
-					}
-                } else {
-					c.value = c.value1;
-				}
-			}
-			
-            console.log("TCL: KonectySearchNode -> filter.conditions", filter.conditions)
-			// Ensure that the condition will have value property
-			filter.conditions = filter.conditions.filter(item => ![null, undefined, NaN].includes(item.value));
-            console.log("TCL: KonectySearchNode -> filter.conditions", filter.conditions)
+        if (c.value1 && c.value1.length) {
+          c.value1 = RED.util.evaluateNodeProperty(
+            c.value1,
+            c.value1Type,
+            this,
+            msg
+          );
+        }
+        console.log("TCL: KonectySearchNode -> c.value1", c.value1);
 
-			let root_url = node.server.host;
-			if (root_url.endsWith('/')) {
-				root_url = root_url.slice(0, root_url.length - 1);
-			}
+        if (!!c.value2) {
+          c.value2 = RED.util.evaluateNodeProperty(
+            c.value2,
+            c.value2Type,
+            this,
+            msg
+          );
+        }
 
-			let token = node.server.key;
-			if (config.token && config.tokenType) {
-				const userToken = RED.util.evaluateNodeProperty(config.token, config.tokenType, this, msg);
-				if (/[^ ]+/.test(userToken)) {
-					token = userToken;
-				}
-            }
+        if (c.operator === "lookup") {
+          c.term += "._id";
+          c.operator = "equals";
+          c.value = c.lookupId;
+        } else if (c.operator === "between") {
+          c.value = {};
+          if (c.value1) {
+            if (c.fieldType == "dateTime")
+              c.value["greater_or_equals"] = {
+                $date: moment(c.value1, "YYYY-MM-DD HH:mm").format()
+              };
+            else if (c.fieldType == "date")
+              c.value["greater_or_equals"] = {
+                $date: moment(c.value1, "YYYY-MM-DD").format()
+              };
+            else if (c.fieldType == "money" || c.fieldType == "number")
+              c.value["greater_or_equals"] = Number(c.value1);
+            else c.value["greater_or_equals"] = c.value1;
+          }
+          if (c.value2) {
+            if (c.fieldType == "dateTime")
+              c.value["less_or_equals"] = {
+                $date: moment(c.value2, "YYYY-MM-DD HH:mm").format()
+              };
+            else if (c.fieldType == "date")
+              c.value["less_or_equals"] = {
+                $date: moment(c.value2, "YYYY-MM-DD").format()
+              };
+            else if (c.fieldType == "money" || c.fieldType == "number")
+              c.value["less_or_equals"] = Number(c.value2);
+            else c.value["less_or_equals"] = c.value2;
+          }
+        } else {
+          c.value = c.value1;
+        }
+      }
 
-            let start = 0;
-            if (config.start && config.startType) {
-                start = RED.util.evaluateNodeProperty(config.start, config.startType, this, msg);
-            }
+      console.log(
+        "TCL: KonectySearchNode -> filter.conditions",
+        filter.conditions
+      );
+      // Ensure that the condition will have value property
+      filter.conditions = filter.conditions.filter(
+        item => ![null, undefined, NaN].includes(item.value)
+      );
+      console.log(
+        "TCL: KonectySearchNode -> filter.conditions",
+        filter.conditions
+      );
 
-			node.status({ fill: 'blue', shape: 'ring', text: 'searching...' });
-			const axios = axios_instance.create({
-				baseURL: root_url,
-				headers: {
-					Authorization: token
-				}
-			});
+      let root_url = node.server.host;
+      if (root_url.endsWith("/")) {
+        root_url = root_url.slice(0, root_url.length - 1);
+      }
 
-            const fields = JSON.parse(config.projections);
-            const params = {
-                filter: JSON.stringify(filter),
-                limit: config.limit && Number(config.limit) || 0,
-                start: Number(start),
-                sort: `[{"property":"_id","direction":"ASC"}]`,
-                fields: Array.isArray(fields) && fields.length > 0 ? fields.join() : undefined
-            };
+      let token = node.server.key;
+      if (config.token && config.tokenType) {
+        const userToken = RED.util.evaluateNodeProperty(
+          config.token,
+          config.tokenType,
+          this,
+          msg
+        );
+        if (/[^ ]+/.test(userToken)) {
+          token = userToken;
+        }
+      }
 
-            if (config.debugMode) {
-                node.warn(params);
-            }
+      let start = 0;
+      if (config.start && config.startType) {
+        start = RED.util.evaluateNodeProperty(
+          config.start,
+          config.startType,
+          this,
+          msg
+        );
+      }
 
-			axios
-				.get(`/rest/data/${config.doc.split(':')[1]}/find`, { params })
-				.then(function(response) {
-					node.status({});
-					msg.success = response.data.success;
-                    msg.payload = response.data.data;
-                    msg.total = response.data.total;
-					node.send(msg);
-				})
-				.catch(function(error) {
-					node.status({ fill: 'red', shape: 'ring', text: 'Error searching' });
-					node.error(error);
-				});
-		});
+      node.status({ fill: "blue", shape: "ring", text: "searching..." });
+      const axios = axios_instance.create({
+        baseURL: root_url,
+        headers: {
+          Authorization: token
+        }
+      });
 
-		node.on('close', function(removed, done) {
-			if (removed) {
-				// This node has been deleted
-			} else {
-				// This node is being restarted
-			}
-			done();
-		});
-	}
+      const fields = JSON.parse(config.projections);
+      const sort = JSON.parse(config.sort || "[]");
 
-	RED.nodes.registerType('konecty-search', KonectySearchNode);
+      const params = {
+        filter: JSON.stringify(filter),
+        limit: (config.limit && Number(config.limit)) || 0,
+        start: Number(start),
+        sort:
+          sort.length === 0
+            ? `[{"property":"_id","direction":"ASC"}]`
+            : JSON.stringify(sort),
+        fields:
+          Array.isArray(fields) && fields.length > 0 ? fields.join() : undefined
+      };
+
+      if (config.debugMode) {
+        node.warn(params);
+      }
+
+      axios
+        .get(`/rest/data/${config.doc.split(":")[1]}/find`, { params })
+        .then(function(response) {
+          node.status({});
+          msg.success = response.data.success;
+          msg.payload = response.data.data;
+          msg.total = response.data.total;
+          node.send(msg);
+        })
+        .catch(function(error) {
+          node.status({ fill: "red", shape: "ring", text: "Error searching" });
+          node.error(error);
+        });
+    });
+
+    node.on("close", function(removed, done) {
+      if (removed) {
+        // This node has been deleted
+      } else {
+        // This node is being restarted
+      }
+      done();
+    });
+  }
+
+  RED.nodes.registerType("konecty-search", KonectySearchNode);
 };
